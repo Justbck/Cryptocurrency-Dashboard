3a47abec23a7080f9062e409dece878499cdf10b
Justbck <62395299+Justbck@users.noreply.github.com>
2020-08-19 14:17:00 +0100

    Add Appbar

---
+++ src/App/AppBar/AppBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+
+
+const Logo = styled.div`
+    font-size:1.5em;
+    
+`
+
+const Bar = styled.div`
+    display:grid;
+    grid-template-columns:180px auto 100px 100px;
+`
+
+const ControlButtonElem = styled.div`
+    cursor:pointer;
+    ${props => props.active && css`
+   
+        color:#3bba9c;
+    `}
+`
+
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+
+}
+
+function ControlButton({name, active}){
+    return (
+    <ControlButtonElem active = {active}>
+        {toProperCase(name)}
+    </ControlButtonElem>
+    )
+}
+
+export default function(){
+    return <Bar>
+
+        <Logo> CryptoDash</Logo>
+        <div/>
+        <ControlButton active name = "dashboard"/>
+        <ControlButton active name = "settings" />
+
+
+    </Bar>
+}
